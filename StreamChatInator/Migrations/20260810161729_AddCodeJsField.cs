a2f5cbdb0bb98bb4b3d9eddf273d1f89268da365
Frederik Gelder <frederik.gelder@freenet.de>
2026-08-10 18:19:55 +0200

    forgot migration

---
+++ StreamChatInator/Migrations/20260810161729_AddCodeJsField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamChatInator.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeJsField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CodeJs",
+                table: "EventFilters",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodeJs",
+                table: "EventFilters");
+        }
+    }
+}
